8e90853f6f6ceef64ebb1ce6d7a9f24d509aea01
Marco Bejarano <marco.bejarano03@gmail.com>
2020-11-23 23:05:13 -0800

    Updated localstorage and on click event

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ var iconurl = "";
 var country = "";
 
 var listOfSearchedCities = [];
+
+// Creating variable...
+var getSearchedCitiesFromLS = JSON.parse(localStorage.getItem("searched-cities"));
+if (getSearchedCitiesFromLS !== null) {
+    getSearchedCitiesFromLS.forEach(function(cit y) {city.toUpperCase();});
+    listOfSearchedCities = getSearchedCitiesFromLS;
+}
+
+$(document).ready(function() {
+    displayCities(listOfSearchedCities);
+    if (getSearchedCitiesFromLS !== null) {
+        var lastCity = listOfSearchedCities[0];
+        searchCity(lastCity);
+    }
+}); 
+
+$("#search-btn").on("click", function() {
+    event.preventDefault();
+    clearDisplayedWeatherInfo()
+    resetGlobalVariables()
+    var cityName = $("input").val().toUpperCase().trim();
+}
