d9df5743bd755821ec3bfe66c0fe4df131cfcf3a
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-29 19:56:18 -0700

    changing html styling

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ else
 {
     locations = locations.split(",")
 };
+
+$("#search").on("click", function() {
+    event.preventDefault();
+    event.stopPropagation();
+    let city = $("#city-input").val().trim();
+    if (city != '') {
+        $("#city-input").html("")
+        searchCity(city);
+        forecast(city);
+        addHistory(city);
+        returnHistory()
+    }
+    else {
+        $("#city-input").html("Input cannot be empty");
+    }
+});
+
+// Recent locations searched
+function addHistory(city){ 
+    locations.push(city);
+    localStorage.setItem("locations", locations); 
+};
