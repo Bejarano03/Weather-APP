98385a180bfecc572f7bd84ae689543748d63cb6
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-29 19:51:56 -0700

    added local storage

--- script.js
+++ script.js
@@ -1,46 +1,8 @@
-var APIKey = "45cd964728f300c81057241ce6ee826d";
-
-
-var listOfSearchedCities = [];
-
-// Creating variable...
-var getSearchedCitiesFromLS = JSON.parse(localStorage.getItem("searched-cities"));
-if (getSearchedCitiesFromLS !== null) {
-    getSearchedCitiesFromLS.forEach(function(city) {city.toUpperCase();});
-    listOfSearchedCities = getSearchedCitiesFromLS;
+let locations = localStorage.getItem("locations");
+if (!locations) {
+    locations = [];
 }
-
-$(document).ready(function() {
-    displayCities(listOfSearchedCities);
-    if (getSearchedCitiesFromLS !== null) {
-        var lastCity = listOfSearchedCities[0];
-        searchCity(lastCity);
-    }
-}); 
-
-$("#search-btn").on("click", function() {
-    event.preventDefault();
-    clearDisplayedWeatherInfo()
-    resetGlobalVariables()
-    var cityName = $("input").val().toUpperCase().trim();
-    $("#search-input").val("");
-    searchCity(cityName);
-
-    if (cityName !== ""&& listOfSearchedCities[0] !== cityName) {
-        listOfSearchedCities.unshift(cityName);
-        localStorage.setItem("searched-cities", JSON.stringify(listOfSearchedCities));
-     if (listOfSearchedCities.length ===1) 
-    
-    
-});
-
-function getWeather (cityName) {
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(response) {
-        console.log(this);
-    }
-}
+else
+{
+    locations = locations.split(",")
+};
